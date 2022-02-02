16f26e5758c23db37e82a70620cb2fcbed9da29e
Benjamin <benjaminrowe0@gmail.com>
2022-02-02 20:56:46 +1100

    added better error handling

--- index.js
+++ index.js
@@ -3,14 +3,19 @@
 const searchButton = document.getElementById('search-button');
 const showinfo = document.getElementById('container-info')
 const showError = document.getElementById('errorMessge')
-const searchTheUser = document.getElementById('searchingUser')
+const searchTheUserButton = document.getElementById('searchingUser')
+const userName = document.getElementById('userName')
 const API_Key = "uPhudeqCwQTA3gwfbquN"
 
 const searchUserinfo = (e) => {
+    // console.log(userName.value)
+    const validatePlayerNameCheck = infoCheckFields(userName.value)
+    if(!validatePlayerNameCheck) {return}
+    ResetPageInfo()
     showInfoBox(false)
     searchingUser()
     const platform = document.getElementById('all-platforms');
-    const userName = document.getElementById('userName');
+    
 
     let userPlatform = platform.value
     let playerName = userName.value 
@@ -27,25 +32,43 @@ const searchUserinfo = (e) => {
 const fetchingData = (userPlatform, player) => {
     fetch(`https://api.mozambiquehe.re/bridge?version=5&platform=${userPlatform}&player=${player}&auth=${API_Key}`)
     .then(resp => resp.json())
-    .then(resp => showStats(resp))
+    .then(resp => ShowResponse(resp))
     .catch(err => ErrorMessage(err))
 }
 
+
+
 const searchingUser = (found = false) =>{
-    if(found){return searchTheUser.classList.add('hideInfo') }
-    if(!found){return searchTheUser.classList.remove('hideInfo') }
+    if(found){return searchTheUserButton.classList.add('hideInfo') }
+    if(!found){return searchTheUserButton.classList.remove('hideInfo') }
 }
 
 
 
 const ErrorMessage = (err) => {
-    console.log(`this is a error message ${err.message}`)
+    searchTheUserButton.classList.add('hideInfo')
+    const errorDiv = document.getElementById('errorMessge')
+    const errorTitle = document.createElement('h1')
+    errorTitle.textContent = err
+    errorDiv.append(errorTitle)
     showInfoBox(false)
     showError.classList.remove('hideInfo')
     setTimeout( () => {
         showError.classList.add('hideInfo')
-    }, 3000);
+        errorTitle.remove()
+    }, 2000);
     
+}
+
+const infoCheckFields = (playerAccount) => {
+    console.log(playerAccount.length)
+    if(playerAccount.length <= 0 ) {
+        ErrorMessage("need to enter username")
+        return false
+    }
+    if(playerAccount.length > 0 ) {
+        return true
+    }
 }
 
 const showInfoBox = (bool) => {
@@ -53,9 +76,27 @@ const showInfoBox = (bool) => {
     if(!bool) {return showinfo.classList.add('hideInfo') }
 }
 
+const showRankImage = (rankImg) => {
+    const container = document.getElementById('container-info')
+    const newRankImg = document.createElement('img')
+    newRankImg.id = "rank-img"
+    newRankImg.src = rankImg
+    container.appendChild(newRankImg)
+}
 
+const showChampImage = (champImg) => {
+    const container = document.getElementById('champ-container')
+    const newChampImg = document.createElement('img')
+    newChampImg.id = "champ-img"
+    newChampImg.src = champImg
+    container.append(newChampImg)
+}
 
-const showStats = (info) => {
+const ShowResponse = (info) => {
+    if(info.Error) {
+        ErrorMessage(info.Error)
+        return
+    }
     showError.classList.add('hideInfo')
     searchingUser(true)
     showInfoBox(true)
@@ -75,8 +116,22 @@ const showStats = (info) => {
     level.textContent = info.global.level
     battlePass.textContent = info.global.battlepass.level
     rank.textContent = `${rankName} and div ${rankDiv} `
+    showRankImage(info.global.rank.rankImg)
+    showChampImage(info.legends.selected.ImgAssets.icon)
     
 }
+
+
+const ResetPageInfo = () => {
+    const rankElement = document.getElementById('rank-img')
+    const champElement = document.getElementById('champ-img')
+    const checkingElements = document.body.contains(rankElement) || document.body.contains(champElement);
+    if(checkingElements){
+        rankElement.remove()
+        champElement.remove()
+    }
+}
+
 
 
 searchButton.addEventListener('click',searchUserinfo)
